b04893ebfb14e7fa0d462612c74ce5ae73950caf
KevinVelassco <kevinvelassco@gmail.com>
2021-08-01 14:48:12 -0500

    feat: get top books method is added

--- server/src/routes/books.router.ts
+++ server/src/routes/books.router.ts
@@ -2,11 +2,13 @@ import {Router} from "express";
 import {CreateBook} from "../controllers/books/create-book.controller";
 import { GetBooksByGenre } from "../controllers/books/get-books-by-genre.controller";
 import {GetBooks} from "../controllers/books/get-books.controller";
+import { GetTopBooks } from "../controllers/books/get-top-books.controller";
 
 const router = Router();
 
 router.get('/', GetBooks);
 router.get('/byGenre/:genreId', GetBooksByGenre);
+router.get('/topBooks', GetTopBooks);
 router.post('/', CreateBook);
 
 export default router
